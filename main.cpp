ca03de5063a1d6218c2605b8324030dfe9df5349
Roman Šostok <102825558+RomanSosty@users.noreply.github.com>
2022-11-01 20:55:12 +0100

    Second change

--- main.cpp
+++ main.cpp
@@ -6,15 +6,22 @@ void ComparisonCountingSort(int *myNum, int lenArr){
     for(int i = 0; i < lenArr; i++)
     {
         count[i] = 0;
-        std::cout << count[i] << std::endl;
     }
 
     for(int i = 0; i < lenArr-1; i++){
-        for (int j = i+1; j < count; j++)
+        for (int j = i+1; j < lenArr; j++)
         {
-            /* code */
+            if(myNum[i] < myNum[j]){
+                count[j] = count[j]+1;
+            }else{
+                count[i] = count[i]+1;
+            }
         }
         
+    }
+
+    for(int i = 0; i <lenArr; i++){
+
     }
 
 }
